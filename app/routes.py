4343b765a34a2635768604d6a6c976d19cc9d066
Will Tholke <76757491+willtholke@users.noreply.github.com>
2021-04-25 00:38:06 -0700

    add form submission capability

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ Rendering converts a template into a complete HTML page using Jinja2.
 
 
 """
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -41,9 +41,13 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me{}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')  # instructs web client browser to navigate away
     return render_template('login.html', title="Sign in", form=form)
 
 
