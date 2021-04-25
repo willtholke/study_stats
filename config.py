9d7a2ac1179b4826b3361df131c583819a8f8339
Will Tholke <76757491+willtholke@users.noreply.github.com>
2021-04-25 14:33:43 -0700

    add database configuration

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ Used with Flask-WTF extension to protect web forms against CSRF
 
 """
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'  # dict of environ variables
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
